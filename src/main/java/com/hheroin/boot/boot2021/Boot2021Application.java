281c62d1884c8841546ba420c888e0fe9e9a05ac
HHeroin <1151416353@qq.com>
2021-07-28 14:07:13 +0800

    project init

---
+++ src/main/java/com/hheroin/boot/boot2021/Boot2021Application.java
@@ -0,0 +1,13 @@
+package com.hheroin.boot.boot2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot2021Application.class, args);
+    }
+
+}
